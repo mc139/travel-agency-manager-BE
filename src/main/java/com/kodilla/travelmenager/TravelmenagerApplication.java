c9a490dcfc913c016949684d06adb72137a5d94d
mc139 <maciejandrzejcieslik@gmail.com>
2021-08-21 18:58:53 +0100

    initial commit

---
+++ src/main/java/com/kodilla/travelmenager/TravelmenagerApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.travelmenager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelmenagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelmenagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/travelmenager/TravelmenagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.travelmenager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelmenagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
